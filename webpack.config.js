7f051e60064c173069ec90b13a572ce2991fc3ae
Kamil Tomšík <info@tomsik.cz>
2017-09-27 00:03:10 +0200

    babel, commonschunk

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,33 @@
 const path = require('path')
+const webpack = require('webpack')
 
 module.exports = {
-  entry: './src',
+  entry: {
+    bundle: './src'
+  },
   output: {
     path: path.resolve(__dirname, 'build'),
     publicPath: 'build',
-    filename: 'bundle.js'
+    filename: '[name].js'
   },
   devServer: {
     contentBase: 'public'
-  }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      }
+    ]
+  },
+  plugins: [
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'vendor',
+      minChunks: function (module) {
+        return module.context && module.context.indexOf("node_modules") !== -1;
+      }
+    })
+  ]
 }
